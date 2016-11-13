1f9c1c8c747f2fde47e0c587de54882d1d4a6939
Nikhil-Nair <nikhil.nair.2793@gmail.com>
2016-11-13 14:09:56 +0530

    -m

---
+++ Chap2/q13.py
@@ -0,0 +1,11 @@
+#print 1-10 using for loop
+print('1-10 usinf "for" loop')
+for i in range(1,11):
+    print(i)
+
+#print 1-10 using while loop
+print('1-10 usinf "while" loop')
+i=1
+while i<11:
+    print(i)
+    i=i+1
---
+++ Chap3/helloFunc.py
@@ -0,0 +1,9 @@
+def hello():
+    print('Howdy')
+    print('Howdy!!!')
+    print('Hello there you rascal!')
+
+hello()
+hello()
+hello()
+hello()
